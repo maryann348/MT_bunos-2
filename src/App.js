1a5de86d86567b50da9e3a69764be58d3e6b627b
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-09-28 18:51:47 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React, {Component} from 'react';
 
-class App extends Component {
-constructor(props) {
-super(props);
-this.state = {
-objects: [
-{ firstname: "Juan", lastname: 'dela Cruz', age: 21 },
-{ firstname: "Mark", lastname: 'Paul', age: 22 },
-{ firstname: "Paul", lastname: 'Mark', age: 23 },
-
-]
-}
-}
 render() {
-return (
-  <div className = "center">
-    <h2>BUNOS NUMBER2</h2>
-    <hr></hr>
-    <br></br>
-    <br></br>
-{this.state.objects.forEach(user => {
-return(this.state.user)
-})} , ""
-</div>
+  let ListNames = []
+  let ListLast = []
+  let ages = []
+  const List = [{
+      firstName: "Juan",
+      secondName: "Dela Cruz",
+      age: 21
+  },
+  {
+      firstName: "Mark",
+      secondName: "Paul",
+      age: 22
+
+  },
+  {
+      firstName: "Paul",
+      secondName: "Mark",
+      age: 23
+  }]
+
+  List.map((l) => <ul key={l.firstName}>
+      {ListNames.push(l.firstName + ", ")}:
+      {ListLast.push(l.secondName + ", ")}:
+      {ages.push(l.age + ", ")} </ul>)
 
 
-)
 
+  return (
+      <div className = "center">
+        <h1>BONUS NUMBER2</h1>
+        <hr>
+        </hr>
+        <br></br><br></br>
+          <h1> Names : </h1>
+          {ListNames}<br />
+          <h1> Last Names:</h1>
+          {ListLast}<br />
+          <h1>Ages :</h1>
+          {ages}
+           <br></br>
+      <br></br>
+      </div>
+     
+  )
 }
 }
 export default App;
